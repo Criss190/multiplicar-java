6c0dcabf192ec6d8ae961a82910bce829b4a54ff
Palacios <samu3l3004@hotmail.com>
2024-08-05 10:28:27 -0500

    se subio la multiplicacion de variables

---
+++ Multiplicar_Numeros/src/main/java/com/mycompany/multiplicar_numeros/MultiplicarNumeros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.multiplicar_numeros;
+import java.util.Scanner;
+
+public class MultiplicarNumeros {
+    Scanner sc =new Scanner(System.in);
+    double a=sc.nextDouble();
+    double b=sc.nextDouble();
+    double resultado = a * b;
+    
+    
+    
+}
